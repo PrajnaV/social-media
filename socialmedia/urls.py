0e75c44e4de01964035819262eff5cf4f609189b
PrajnaV <prajnav2005@gmail.com>
2024-06-25 08:43:54 +0530

    Added static files

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('media/',include('media_app.urls'))
+    path('',include('media_app.urls'))
 ]
