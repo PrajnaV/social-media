a025b76c2a9a474fd840bf0d52856dcd42237020
PrajnaV <prajnav2005@gmail.com>
2024-06-28 22:28:09 +0530

    Upload pics

--- media_app/views.py
+++ media_app/views.py
@@ -91,3 +91,18 @@ def settings(request):
         return redirect('settings')
         
     return render(request,'setting.html',{'user_profile':user_profile})
+
+@login_required(login_url='signin')  
+def upload(request):
+    if request.method=='POST':
+        user = request.user.username
+        image = request.FILES.get('post_img')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user,image=image,caption=caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/') 
+
